032a331645f67ba2b5ea5e5c0ba52f04fd8a8cf5
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 18:25:47 +0200

    Add structure of a method comment block

--- Banking_App_Methods/src/main/java/com/mycompany/banking_app_methods/Banking_App_Methods.java
+++ Banking_App_Methods/src/main/java/com/mycompany/banking_app_methods/Banking_App_Methods.java
@@ -6,3 +6,22 @@ public class Banking_App_Methods {
         
     }
 }
+
+/*
+    A method is a block of code that performs a specific task.
+    Think of it like a mini-program inside your program.
+ 
+    A method has 3 parts 
+    1. Method Signature / Header - Contains info about the method
+        - 4 pieces of data 
+            - Access Specifier (public / private)
+            - Version (static / non-Static) 
+            - void / dataType of the return value being shared
+            - methodName(parameterList) 
+            - Parameter List: (dataType parameterName, dataType parameterName, dataType parameterName) 
+    2. Method Body {}
+            - contain all the code / instructions that method will complete
+    3. Method Return 
+            - A keyword (return) that sits within the {} & will indicate what the method is sharing / return back 
+            - The datatype used for the return will match the dataType seen in the method header
+*/
