02ecb1ab25975bc9efc95b8fd27e14153f86db0c
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-03-10 20:01:11 +0200

    Create new method to print balance

--- Banking_App_Methods/src/main/java/com/mycompany/banking_app_methods/Banking_App_Methods.java
+++ Banking_App_Methods/src/main/java/com/mycompany/banking_app_methods/Banking_App_Methods.java
@@ -4,9 +4,16 @@ public class Banking_App_Methods
 {
 
     // Method: Deposit
-    public static double deposit(double balance, double amount)
+    public static double deposit(double balance, double amount) // Set parameters
     {
         double newBalance = balance + amount;
+        return newBalance;
+    }
+    
+    // Method: Display balance(parameter but no return)
+    public static void displayBalance(double balance)
+    {
+        System.out.println("Current Balance " + balance);
     }
     
     public static void main(String[] args) 
@@ -20,11 +27,16 @@ public class Banking_App_Methods
         // To use the method, we hate to call/invoke the method by its name
         // Syntax: method();
         displayInfo();
+        // Call deposit method(Shares 2 pieces of information)
+        balance = deposit(balance, deposit); // Pass arguements
+        
+        // Print balance in output
+        displayBalance(balance);
     } // closing brace for the main method
     
     // Method with no parameters & no return
     public static void displayInfo()
-    {
+    {   
         System.out.println("ABC BANK");
         System.out.println("123 Happy Place");
         System.out.println("Welcome to your app!");
