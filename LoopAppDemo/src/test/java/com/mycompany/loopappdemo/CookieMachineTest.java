68177d9432d16f094c6b6c9dbb970350e74207ee
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-04-14 18:49:36 +0200

    Create CookieMachineTest.java

---
+++ LoopAppDemo/src/test/java/com/mycompany/loopappdemo/CookieMachineTest.java
@@ -0,0 +1,23 @@
+package com.mycompany.loopappdemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CookieMachineTest {
+    
+    public CookieMachineTest() {
+    }
+
+    @Test
+    public void testBakeCookies() {
+    }
+
+    @Test
+    public void testTasteCookies() {
+    }
+
+    @Test
+    public void testPackCookies() {
+    }
+    
+}
