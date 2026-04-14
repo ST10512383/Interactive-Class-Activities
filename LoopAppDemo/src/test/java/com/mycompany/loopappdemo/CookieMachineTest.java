61a687b0840fab2a8eb35c1c8334faaad8ffc57f
Cailen Boshoff <ST10512383@myemeris.edu.za>
2026-04-14 19:51:16 +0200

    Create tests for cookie machine

--- LoopAppDemo/src/test/java/com/mycompany/loopappdemo/CookieMachineTest.java
+++ LoopAppDemo/src/test/java/com/mycompany/loopappdemo/CookieMachineTest.java
@@ -10,10 +10,29 @@ public class CookieMachineTest {
 
     @Test
     public void testBakeCookies() {
+        // Arrange: create object
+        CookieMachine machine = new CookieMachine();
+        
+        //Act: call method
+        machine.bakeCookies();
+        
+        // Assert: check expected result
+        // Expect 0 ingredients left after baking
+        assertEquals(0, machine.ingredients);
+        assertFalse(machine.ingredients > 0); // Expect to fail if its false
     }
 
     @Test
     public void testTasteCookies() {
+        // Arrange: create object
+        CookieMachine machine = new CookieMachine();
+        
+        // Act: Execute the method
+        machine.tasteCookies();
+        
+        // Assert: Verify the result
+        assertEquals(5, machine.ingredients); // Ingredients should remain unchanged
+        assertTrue(machine.ingredients == 5);
     }
 
     @Test
