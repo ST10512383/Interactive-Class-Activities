d7fce4ca5c2ac0aeed31bd6b14cfd682d2309a8a
ST10512383 <ST10512383@myemeris.edu.za>
2026-08-11 20:15:53 +0200

    Create Dog.java

---
+++ Java-Programming/Semester-Two/InheritanceDemoTwo/src/main/java/com/mycompany/inheritancedemotwo/Dog.java
@@ -0,0 +1,17 @@
+package com.mycompany.inheritancedemotwo;
+
+public class Dog extends Pet {
+
+    public Dog() {
+
+    }
+
+    public Dog(String name, int age) {
+        super.Pet(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        super.makeSound();
+    }
+}
